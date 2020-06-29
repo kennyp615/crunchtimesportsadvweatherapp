bfae38e05359fd8ab3cf47dd54c450fbbb27e9a1
kennyp615 <kennethpruitt3@gmail.com>
2020-06-28 23:05:19 -0500

    latest update to main.js

--- main.js
+++ main.js
@@ -37,7 +37,12 @@ function getWeather() {
 
 
 function showResults(data) {
-    return "<p>Temperature: " + data.main.temp + "&deg;F</p>" +
+    return '<h2>Current Weather for ' + data.name + ', </h2>' +
+        "<p>Sunrise: " + data.sys.sunrise + "</P>" +
+        "<p>Weather Id: " + data.weather.id + "</P>" +
+        "<p>Weather Main: " + data.weather.main + "</P>" +
+        "<p>Weather Description: " + data.weather.description + "</P>" +
+        "<p>Temperature: " + data.main.temp + "&deg;F</p>" +
         "<p>Pressure: " + data.main.pressure + "</P>" +
         "<p>Feels Like: " + data.main.feels_like + "&deg;F</p>" +
         "<p>Humidity: " + data.main.humidity + "</P>" +
@@ -45,7 +50,11 @@ function showResults(data) {
         "<p>Temperature High: " + data.main.temp_max + "&deg;F</p>" +
         "<p>Wind Speed: " + data.wind.speed + "</p>" +
         "<p>Wind Degree: " + data.wind.deg + "</p>" +
-        "<p>Wind Gust: " + data.wind.gust + "</p>";
+        "<p>Wind Gust: " + data.wind.gust + "</p>" +
+        "<p>Clouds: " + data.clouds.all + "&deg;%</p>" +
+        "<p>Sunset: " + data.sys.sunset + "</P>";
+
+
 
 
 
